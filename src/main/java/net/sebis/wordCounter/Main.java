46dd892659f835d01cb4f5901f920f5f2461cb07
ItsSebis <sebi@sebis.net>
2023-07-01 20:18:42 +0200

    list files

--- src/main/java/net/sebis/wordCounter/Main.java
+++ src/main/java/net/sebis/wordCounter/Main.java
@@ -1,19 +1,34 @@
 package net.sebis.wordCounter;
 
+import java.io.File;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        new Main();
+    }
+
+    public Main() {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+8.
-            System.out.println("i = " + i);
+        System.out.println("Hello and welcome!");
+        try {
+            System.out.println(listFilesUsingJavaIO("new"));
+        } catch (NullPointerException ex) {
+            new File("new").mkdirs();
         }
+
+    }
+
+    public Set<String> listFilesUsingJavaIO(String dir) {
+        return Stream.of(Objects.requireNonNull(new File(dir).listFiles()))
+                .filter(file -> !file.isDirectory())
+                .map(File::getName)
+                .collect(Collectors.toSet());
     }
 }
